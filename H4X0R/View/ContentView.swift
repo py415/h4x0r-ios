f678acef6b98aedda67fad30ce2bf5072d672884
Philip Yu <philipyu1997@gmail.com>
2020-04-10 23:11:48 -0700

    Initial Commit

---
+++ H4X0R/View/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R
+//
+//  Created by Philip Yu on 4/10/20.
+//  Copyright © 2020 Philip Yu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
